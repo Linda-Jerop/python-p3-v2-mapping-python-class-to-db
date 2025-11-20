edbf48b59d6d01ae900c285e3676e4681a63ee63
Linda <jeropkoimur@gmail.com>
2025-11-20 16:31:30 +0300

    mapping py class

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,62 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist the attributes of Department instances"""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists Department instances"""
+        sql = """
+            DROP TABLE IF EXISTS departments;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """Insert a new row with the name and location values of the current Department instance.
+        Update object id attribute using the primary key value of new row.
+        """
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))  # Bind parameters to prevent SQL injection
+        CONN.commit()
+        self.id = CURSOR.lastrowid  # Assign the primary key value to the object's id attribute
+
+    @classmethod
+    def create(cls, name, location):
+        """Initialize a new Department instance and save the object to the database"""
+        department = cls(name, location)  # Create new Department object
+        department.save()  # Persist to database
+        return department
+
+    def update(self):
+        """Update the table row corresponding to the current Department instance."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))  # Bind parameters for safe query
+        CONN.commit()
+
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance"""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))  # Bind id parameter
+        CONN.commit()
